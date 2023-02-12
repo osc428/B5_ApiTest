5b25917c605b8c50d6fa80e4e5dc5f02f7f1e772
Osman <osmansceyhan@gmail.com>
2023-02-12 16:10:05 +0100

    First Commit

--- src/test/java/apiTest/day05/JsonToJavaCollection.java
+++ src/test/java/apiTest/day05/JsonToJavaCollection.java
@@ -1,4 +1,47 @@
 package apiTest.day05;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+
 public class JsonToJavaCollection {
+
+    @BeforeClass
+    public void beforeClass() {
+        baseURI = "http://eurotech.study";
+    }
+
+    @Test
+    public void test1() {
+        Response response = RestAssured.given().queryParam("id", 25).when().get("api/profile/userQuery");
+
+        Assert.assertEquals(response.statusCode(), 200);
+
+        Map<String, Object> jsonDataMap = response.body().as(Map.class);
+        System.out.println("jsonDataMap = " + jsonDataMap);
+    }
+
+    @Test
+    public void test2() {
+        Response response = RestAssured.get("api/profile");
+        Assert.assertEquals(response.statusCode(), 200);
+
+        //  de-serialization
+        List<Map<String ,Object>> allBody = response.body().as(List.class);
+        //System.out.println("allBody = " + allBody);
+
+        // print and verify name of the company as "NinjaTeam"
+        System.out.println("allBody.get(28).get(\"company\") = " + allBody.get(28).get("company"));
+        Assert.assertEquals(allBody.get(28).get("company"), "NinjaTeam");
+
+        Map<String, Object> secondUserInfo = allBody.get(1);
+        System.out.println("secondUserInfo = " + secondUserInfo);
+    }
 }
